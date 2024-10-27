e4df9265a4a6da5766ef74d0ae1ab469f7fb403b
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-10-27 10:38:05 +0530

    feat:added new question

---
+++ testGorund/test8.js
@@ -0,0 +1,16 @@
+function calculateSaving(days) {
+  const checkWeeks = Math.floor(days / 7);
+  const restDays = days % 7;
+  let totalSavings = 0;
+  for (var i = 0; i < checkWeeks; i++) {
+    for (let j = 1; j <= 7; j++) {
+      totalSavings += j + i;
+    }
+  }
+  for (let z = 1; z <= restDays; z++) {
+    totalSavings += z + i;
+  }
+  console.log(totalSavings);
+}
+
+calculateSaving(9);
