e40cef6a9342929264da1761ffb078ac762e95c2
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-10-27 19:19:35 +0530

    feat:added new questions

---
+++ _others/typedStr.js
@@ -0,0 +1,43 @@
+
+
+const s = "ab#c";
+const t = "ad#c";
+
+function typedString(s, t) {
+  let left = s.length - 1;
+  let right = t.length - 1;
+
+  while (left >=0 || right>=0) {
+    if (s[left] == "#" || t[right] == "#") {
+      if (s[left] == "#") {
+        let cout_for_skip = 2;
+        while (cout_for_skip !== 0) {
+          if (s[left] == "#") {
+            cout_for_skip += 2;
+          }
+          left--;
+          cout_for_skip--;
+        }
+      }
+      if (s[right] == "#") {
+        let cout_for_skip = 2;
+        while (cout_for_skip !== 0) {
+          if (s[right] == "#") {
+            cout_for_skip += 2;
+          }
+          right--;
+          cout_for_skip--;
+        }
+      }
+    } else {
+      if (s[left] !== t[right]) {
+        return false;
+      } else {
+        left--;
+        right--;
+      }
+    }
+    return true
+  }
+}
+console.log(typedString(s, t));
