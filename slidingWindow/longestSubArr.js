2dec375c8560f53ace16abd00a88e62070d98127
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-10-28 18:31:17 +0530

    feat added new questions

--- slidingWindow/longestSubArr.js
+++ slidingWindow/longestSubArr.js
@@ -1,27 +1,26 @@
-const arr = [4, 3, 1, 2, 2, 5,1,1, 8];
+const arr = [4, 3, 1, 2, 2, 5, 1, 1, 8];
 const k = 10;
-let i =0;
+let i = 0;
 
-function longestSubArr(arr,k){
-    let sum = 0;
-    let longest =0;
+function longestSubArr(arr, k) {
+  let sum = 0;
+  let longest = 0;
 
-    for(var j=0;j<arr.length;j++){
+  for (var j = 0; j < arr.length; j++) {
+    const el = arr[j];
 
-        const el = arr[j];
+    sum += el;
 
-        sum += el;
-
-        while(sum >= k){
-            sum -= arr[i];
-            i++;
-        }
-        
-        longest = Math.max(longest,j - i +1);   
+    while (sum >= k) {
+      sum -= arr[i];
+      i++;
     }
-    
-   console.log(arr.slice(i,j-1));
-    return longest
+
+    longest = Math.max(longest, j - i + 1);
+  }
+
+  console.log(arr.slice(i, j - 1));
+  return longest;
 }
 
-console.log(longestSubArr(arr,k));
+console.log(longestSubArr(arr, k));
