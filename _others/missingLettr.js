7cc4960fb16c40ea99d89bbd8ed025f68f746dad
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-11-24 21:31:29 +0530

    feat:added missing letter question

---
+++ _others/missingLettr.js
@@ -0,0 +1,17 @@
+const arr = ["a", "b", "c", "e"];
+
+function findFirstMissingLetter(arr) {
+  for (let i = 0; i < arr.length; i++) {
+
+    const char = arr[i];
+    const next_char = arr[i+1];
+
+    if(i< arr.length -1 && char.charCodeAt(0) + 1 !== next_char.charCodeAt(0)){
+        return String.fromCharCode(char.charCodeAt(0)+1)
+    }
+
+  }
+  return "no missing"
+}
+
+console.log(findFirstMissingLetter(arr));
